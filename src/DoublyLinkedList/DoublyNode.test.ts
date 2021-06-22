3d48e47f7629c7da6b62902b8a6af9d02e9aca91
agusthas <suzumantan@gmail.com>
2021-06-22 15:30:49 +0700

    DoublyNode

--- src/DoublyLinkedList/DoublyNode.test.ts
+++ src/DoublyLinkedList/DoublyNode.test.ts
@@ -3,52 +3,40 @@ import DoublyNode from './DoublyNode';
 describe('DoublyNode unit tests', () => {
   const n1 = new DoublyNode<number>(1);
   const n2 = new DoublyNode<number>(2);
-  const n3 = new DoublyNode<number>(3);
   describe('new DoublyNode()', () => {
     it('should create an instance of DoublyNode', () => {
       expect(n1).toBeInstanceOf(DoublyNode);
       expect(n2).toBeInstanceOf(DoublyNode);
-      expect(n3).toBeInstanceOf(DoublyNode);
     });
   });
 
-  describe('.getData()', () => {
-    it('should get the data', () => {
-      expect(n1.getData()).toBe(1);
-      expect(n2.getData()).toBe(2);
-      expect(n3.getData()).toBe(3);
+  describe('.data', () => {
+    it('set', () => {
+      n1.data = 2;
+    });
+
+    it('get', () => {
+      expect(n1.data).toBe(2);
     });
   });
 
-  describe('.setData()', () => {
-    it('should update the data', () => {
-      n1.setData(10);
-      expect(n1.getData()).toBe(10);
+  describe('.next', () => {
+    it('set', () => {
+      n1.next = n2;
+    });
+
+    it('get', () => {
+      expect(n1.next).toBe(n2);
     });
   });
 
-  describe('.setNext()', () => {
-    it('should set the next node', () => {
-      const next = n1.setNext(n2);
-      expect(next).toBe(n1);
+  describe('.prev', () => {
+    it('set', () => {
+      n2.prev = n1;
     });
-  });
 
-  describe('.getNext()', () => {
-    it('should get the next node', () => {
-      expect(n1.getNext()).toBe(n2);
-    });
-  });
-
-  describe('.setPrev()', () => {
-    it('should set previous node', () => {
-      expect(n2.setPrev(n1)).toBe(n2);
-    });
-  });
-
-  describe('.getPrev()', () => {
-    it('should get previous node', () => {
-      expect(n2.getPrev()).toBe(n1);
+    it('get', () => {
+      expect(n2.prev).toBe(n1);
     });
   });
 });
--- src/DoublyLinkedList/DoublyNode.ts
+++ src/DoublyLinkedList/DoublyNode.ts
@@ -3,44 +3,45 @@ export default class DoublyNode<T> {
   private _prev: DoublyNode<T> | null;
   private _next: DoublyNode<T> | null;
 
-  constructor(data: T, prev = null, next = null) {
+  constructor(
+    data: T,
+    prev: DoublyNode<T> | null = null,
+    next: DoublyNode<T> | null = null
+  ) {
     this._data = data;
     this._prev = prev;
     this._next = next;
   }
 
-  setData(value: T): DoublyNode<T> {
+  set data(value: T) {
     this._data = value;
-    return this;
   }
 
-  getData(): T {
+  get data(): T {
     return this._data;
   }
 
-  setPrev(node: DoublyNode<T> | null): DoublyNode<T> {
+  set prev(node: DoublyNode<T> | null) {
     if (node && !(node instanceof DoublyNode)) {
       throw new Error('setPrev expects a DoublyNode or null');
     }
 
     this._prev = node;
-    return this;
   }
 
-  getPrev(): DoublyNode<T> | null {
+  get prev(): DoublyNode<T> | null {
     return this._prev;
   }
 
-  setNext(node: DoublyNode<T> | null): DoublyNode<T> {
+  set next(node: DoublyNode<T> | null) {
     if (node && !(node instanceof DoublyNode)) {
       throw new Error('setNext expects a DoublyNode or null');
     }
 
     this._next = node;
-    return this;
   }
 
-  getNext(): DoublyNode<T> | null {
+  get next(): DoublyNode<T> | null {
     return this._next;
   }
 }
